0616198fd7f8e4e1afe77cab90b92b19587a57d2
Dabrovski <wojciech.dabrovski@gmail.com>
2025-09-03 22:35:33 +0200

    feat: create companies model

---
+++ app/models/company.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Company < ApplicationRecord
+  validates :name, presence: true
+end
